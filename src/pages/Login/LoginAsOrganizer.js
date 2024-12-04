aa8fe776b1c7e1269372326dd6b5a1399bba17fe
Ankit Kumar <ankitk8006@gmail.com>
2024-12-04 14:16:51 +0530

    login problem

--- src/pages/Login/LoginAsOrganizer.js
+++ src/pages/Login/LoginAsOrganizer.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react'
 
 export const LoginAsOrganizer = () => {
   return (
     
-    <main>
-              
-        <form  className='flex flex-col m-20 p-20 border-2 rounded-xl bg-slate-600' >
-          <div className="mb-6 flex w-full">
-              <label htmlFor="email" className="block mb-2 text-sm font-medium text-white dark:text-gray-300">Your email</label>
-              <input  type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Organizer@gamil.com" required autoComplete="off" />
+      <div className='p-8 w-3/4'>        
+        <form className='w-full' >
+        <div className="flex gap-4 flex-row mb-6 items-center justify-evenly">
+              <label htmlFor="name" className="block mb-2 text-sm font-medium  text-gray-300 w-16">Name</label>
+              <input type="name" id="name" className="shadow-sm bg-gray-700 border border-gray-400 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light w-full" placeholder="Shubham Sarda" required autoComplete="off" />
           </div>
-          <div className="mb-6 flex">
-              <label htmlFor="password" className="block mb-2 text-sm font-medium text-white dark:text-gray-300">Your password</label>
-              <input  type="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required  placeholder='xyz@1243'/>
+          <div className="flex gap-4 flex-row items-center justify-evenly mb-6">
+              <label htmlFor="email" className="block mb-2 text-sm font-medium  text-gray-300 w-16">Pass</label>
+              <input type="email" id="password" className="shadow-sm bg-gray-700 border border-gray-400 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light w-full " placeholder="shubham.com" required autoComplete="off" />
           </div>
           <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Log In</button>
         </form>
-       
-    </main>
-
+        </div> 
   )
 }
